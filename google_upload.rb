50944901afc61b0034b83d5e49b47993352c0272
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-05-08 14:08:40 +0300

    Save result to html and open with browser

--- google_upload.rb
+++ google_upload.rb
@@ -2,17 +2,33 @@
 
 require 'base64'
 
-file = File.open('icon.jpg', 'rb').read
+file_name = ARGV.shift || (puts "Provide file name"; exit 1)
 
-img = Base64.strict_encode64(file).chomp
+unless (File.exists?(file_name))
+  puts "File not found :("
+  exit 2
+end
+
+file = File.open(file_name, 'rb').read
+
+img = Base64.strict_encode64(file)
 
 img.gsub!('+', '-').gsub!('/', '_').gsub!('.', '=')
 
+html = <<DATA
+<html>
+<head><title>Google Image</title></head><body>
+<form id="f" method="POST" action="https://www.google.com/searchbyimage/upload"     enctype="multipart/form-data">
+<input type="hidden" name="image_content" value="#{img}">
+<input type="hidden" name="filename" value="">
+<input type="hidden" name="image_url" value="">
+<input type="hidden" name="sbisrc" value="cr_1_5_1">
+</form>
+<script>document.getElementById("f").submit();</script></body></html>
+DATA
 
-img = '<html><head><title>Google Image</title></head><body><form id="f" method="POST" action="https://www.google.com/searchbyimage/upload" enctype="multipart/form-data"><input type="hidden" name="image_content" value="' +
-  img + '"><input type="hidden" name="filename" value=""><input type="hidden" name="image_url" value=""><input type="hidden" name="sbisrc" value="cr_1_5_1"></form><script>document.getElementById("f").submit();</script></body></html>';
+out = File.open('out.html', 'wb')
+out.write(html)
+out.close
 
-
-f = "data:text/html;charset=utf-8;base64,#{Base64.strict_encode64(img).chomp}"
-
-puts f
+`open out.html`
